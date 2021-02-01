adffbd73cfa386629dcb853809b18c24dd71f318
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-01 17:04:20 +0100

    execute basic https protocol get request for openWeatherMap api

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
-app.get("/", function(req, res){
-      res.send("server is up and running")
-})
+//runs in browser (home)
+app.get("/", function (req, res) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=Barcelona&APPID=b7eabe995546dde5f08786249f6b2228&units=metric";
 
+  https.get(url, function (response) {
+    console.log(response.statusCode);
+  });
 
-app.listen(3000, function() {
-      console.log('server is running');
-})
+  res.send("server is running bread bin");
+});
+
+//runs in terminal
+app.listen(3000, function () {
+  console.log("server is running");
+});
