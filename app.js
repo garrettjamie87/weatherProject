2bb27ea076756d7d5eed055bee1144c7a5c8b9ac
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-02 11:50:16 +0100

    add html input field

--- app.js
+++ app.js
@@ -5,28 +5,36 @@ const app = express();
 
 //runs in browser (home)
 app.get("/", function (req, res) {
-  
-  const query = "Barcelona";  
-  const apiKey = "b7eabe995546dde5f08786249f6b2228"
-  const unit = 'metric'
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&APPID=" + apiKey + "&units=" + unit;
-
-  https.get(url, function (response) {
-    //     console.log(response.statusCode);
-
-    response.on("data", function (data) {
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const weatherDescription = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-      res.write(`<p>the weather is currently ${weatherDescription}</p>`);
-      res.write(`<h1>The temp in BCN is ${temp} degrees Celcius.</h1>`);
-      res.write(`<img src=${imageURL}>`);
-      res.send();
-    });
-  });
+  res.sendFile(__dirname + "/index.html");
+ 
 });
+
+// const query = "Barcelona";  
+// const apiKey = "b7eabe995546dde5f08786249f6b2228"
+// const unit = 'metric'
+// const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&APPID=" + apiKey + "&units=" + unit;
+
+// https.get(url, function (response) {
+//   //     console.log(response.statusCode);
+
+//   response.on("data", function (data) {
+//     const weatherData = JSON.parse(data);
+//     const temp = weatherData.main.temp;
+//     const weatherDescription = weatherData.weather[0].description;
+//     const icon = weatherData.weather[0].icon;
+//     const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+//     res.write(`<p>the weather is currently ${weatherDescription}</p>`);
+//     res.write(`<h1>The temp in BCN is ${temp} degrees Celcius.</h1>`);
+//     res.write(`<img src=${imageURL}>`);
+//     res.send();
+//   });
+// });
+
+
+
+
+
+
 
 //runs in terminal
 app.listen(3000, function () {
