d478b667eaa5da2cfe2b3183a20f43b794801139
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-02 12:11:15 +0100

    add everything to post request

--- app.js
+++ app.js
@@ -4,43 +4,42 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
- 
 });
 
-app.post("/", function (req,res){
-      console.log('postman pat in da house')
-})
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  const apiKey = "b7eabe995546dde5f08786249f6b2228";
+  const unit = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&APPID=" +
+    apiKey +
+    "&units=" +
+    unit;
 
-// const query = "Barcelona";  
-// const apiKey = "b7eabe995546dde5f08786249f6b2228"
-// const unit = 'metric'
-// const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&APPID=" + apiKey + "&units=" + unit;
+  https.get(url, function (response) {
+    //     console.log(response.statusCode);
 
-// https.get(url, function (response) {
-//   //     console.log(response.statusCode);
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write(`<p>the weather is currently ${weatherDescription}</p>`);
+      res.write(`<h1>The temp in ${query} is ${temp} degrees Celcius.</h1>`);
+      res.write(`<img src=${imageURL}>`);
+      res.send();
+    });
+  });
+});
 
-//   response.on("data", function (data) {
-//     const weatherData = JSON.parse(data);
-//     const temp = weatherData.main.temp;
-//     const weatherDescription = weatherData.weather[0].description;
-//     const icon = weatherData.weather[0].icon;
-//     const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-//     res.write(`<p>the weather is currently ${weatherDescription}</p>`);
-//     res.write(`<h1>The temp in BCN is ${temp} degrees Celcius.</h1>`);
-//     res.write(`<img src=${imageURL}>`);
-//     res.send();
-//   });
-// });
-
-
-
-
-
-
+//
 
 //runs in terminal
 app.listen(3000, function () {
